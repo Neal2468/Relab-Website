63cbeb0706455f26f3081c6c9231f06cb4f9fde5
neal2468 <boy.randy33@gmail.com>
2026-05-13 16:04:01 +0200

    video nochmal

--- src/App.tsx
+++ src/App.tsx
@@ -976,7 +976,7 @@ export default function App() {
           onHoverChange={setIsHoveringPlaceholder}
           entrance="slide-right"
           imageSrc="/Media/ring rider.jpg"
-          videoSrc="/Media/Ringrider.mp4"
+          videoSrc="/Media/ringrider.mp4"
         />
         <MassivePlaceholder 
           id="focus-visor"
